d75ca6d148bcf44a69614e7dadd75e7ab3ce3b8f
Tjorriemorrie <jacoj82@gmail.com>
2015-03-12 11:35:25 +0200

    75.5 - 7 added

---
+++ 16_rf_ma/stable-75.5-7/main.py
@@ -0,0 +1,118 @@
+import logging
+import os
+import pickle
+import operator
+import gzip
+import pandas as pd
+import numpy as np
+
+
+def loadData(currency, interval, split):
+    logging.info('Data: loading {0} at {1}...'.format(currency, interval))
+    df = pd.read_csv(
+        r'{0}/../../data/{1}e{2}.csv'.format(os.path.realpath(os.path.dirname(__file__)), currency, interval),
+        names=['date', 'time', 'open', 'high', 'low', 'close', 'volume'],
+        parse_dates=[[0, 1]],
+        index_col=0,
+    ).astype(float)
+    logging.info('Data: {0} rows loaded'.format(len(df)))
+
+    # split
+    if split == 'train':
+        df = df[:int(len(df) * 0.60)]
+    elif split == 'test':
+        df = df[-int(len(df) * 0.40):]
+    else:
+        raise Exception('Unknown split {0}'.format(split))
+
+
+    logging.info('Data: {0} rows used for {1}'.format(len(df), split))
+    # df = df[-2000:]
+    return df
+
+
+def loadQ(currency, interval):
+    logging.info('Q: loading...')
+    try:
+        filename = '{0}/models/{1}_{2}.pklz'.format(os.path.realpath(os.path.dirname(__file__)), currency, interval)
+        with gzip.open(filename) as fz:
+            q = pickle.load(fz)
+    except (IOError, EOFError) as e:
+        logging.error('Could not load Q for {0}'.format(currency))
+        q = {}
+    logging.info('Q: loaded {0}'.format(len(q)))
+    return q
+
+
+def saveQ(currency, interval, q):
+    logging.info('Q: saving...')
+    filename_tmp = '{0}/models/tmp.pklz'.format(os.path.realpath(os.path.dirname(__file__)))
+    filename = '{0}/models/{1}_{2}.pklz'.format(os.path.realpath(os.path.dirname(__file__)), currency, interval)
+    with gzip.open(filename_tmp, 'wb') as fz:
+        pickle.dump(q, fz)
+        os.rename(filename_tmp, filename)
+    logging.info('Q: saved {0}'.format(len(q)))
+
+
+def getBackgroundKnowledge(df, periods):
+    logging.info('Background knowledge: retrieving...')
+
+    # HLC
+    hlc = df.apply(lambda x: (x['high'] + x['low'] + x['close']) / 3, axis=1)
+
+    for x in periods:
+        avg_x = pd.rolling_mean(hlc, x)
+        avg_x_yesterday = avg_x.shift(+1)
+        df['ma_{0}_bullish'.format(x)] = avg_x >= avg_x_yesterday
+        avg_x_delta = abs(avg_x - avg_x_yesterday)
+        avg_x_delta_yesterday = avg_x_delta.shift(+1)
+        df['ma_{0}_divergence'.format(x)] = avg_x_delta >= avg_x_delta_yesterday
+        df['ma_{0}_magnitude'.format(x)] = avg_x_delta > avg_x_delta.mean()
+
+    for x in periods:
+        for y in periods:
+            if y <= x:
+                continue
+            logging.info('MA for {0} and {1}'.format(x, y))
+            avg_x = pd.rolling_mean(hlc, x)
+            avg_y = pd.rolling_mean(hlc, y)
+            df['ma_{0}_crossover_{1}_bullish'.format(x, y)] = avg_x >= avg_y
+
+            ma_diff = avg_x - avg_y
+            avg_x_yesterday = avg_x.shift(+1)
+            avg_y_yesterday = avg_y.shift(+1)
+            ma_diff_yesterday = avg_x_yesterday - avg_y_yesterday
+            df['ma_{0}_crossover_{1}_divergence'.format(x, y)] = ma_diff >= ma_diff_yesterday
+            df['ma_{0}_crossover_{1}_magnitude'.format(x, y)] = ma_diff >= ma_diff.mean()
+
+    logging.info('Background knowledge: retrieved')
+    return df
+
+
+def summarizeActions(q):
+    summary_total = {}
+    summary_count = {}
+    for key, value in q.iteritems():
+        state, action = key.split('|')
+        # total
+        action_total = summary_total.get(action, 0)
+        action_total += value
+        action_total /= 2
+        summary_total[action] = action_total
+
+        action_count = summary_count.get(action, 0)
+        action_count += 1
+        summary_count[action] = action_count
+
+    summary_sorted = sorted(summary_total.items(), key=operator.itemgetter(1))
+
+    for action, info in summary_sorted:
+        logging.error('{0:10s} after {2} states with {1:.4f} avg'.format(action, info, summary_count[action]))
+
+
+def calculateActions(min_trail):
+    actions = []
+    for n in xrange(min_trail, min_trail+50, 5):
+        actions.append('buy-{0}'.format(n))
+        actions.append('sell-{0}'.format(n))
+    return actions
---
+++ 16_rf_ma/stable-75.5-7/world.py
@@ -0,0 +1,103 @@
+import logging
+
+
+DATA = [
+    {'currency': 'AUDUSD', 'trail': 60, 'intervals': [1440], 'pip_mul': 10000},
+
+    {'currency': 'EURGBP', 'trail': 50, 'intervals': [1440], 'pip_mul': 10000},
+
+    {'currency': 'EURUSD', 'trail': 15, 'intervals': [1440], 'pip_mul': 10000},
+
+    {'currency': 'EURJPY', 'trail': 150, 'intervals': [1440], 'pip_mul': 100},
+
+    {'currency': 'GBPUSD', 'trail': 40, 'intervals': [1440], 'pip_mul': 10000},
+
+    {'currency': 'GBPJPY', 'trail': 150, 'intervals': [1440], 'pip_mul': 100},
+
+    {'currency': 'NZDUSD', 'trail': 60, 'intervals': [1440], 'pip_mul': 10000},
+    # 7 mar 40
+    # 9 mar 60
+
+    {'currency': 'USDCAD', 'trail': 60, 'intervals': [1440], 'pip_mul': 10000},
+
+    {'currency': 'USDCHF', 'trail': 50, 'intervals': [1440], 'pip_mul': 10000},
+    # 7 mar 40
+    # 9 mar 50
+
+    {'currency': 'USDJPY', 'trail': 30, 'intervals': [1440], 'pip_mul': 100},
+]
+
+PERIODS = [3, 5, 8, 13, 21, 34, 55]
+
+
+def getState(df, periods):
+    logging.info('State: periods {0}...'.format(periods))
+
+    s = []
+    row = df.iloc[0]
+
+    for x in periods:
+        s.append('1' if row['ma_{0}_bullish'.format(x)] else '0')
+        s.append('1' if row['ma_{0}_divergence'.format(x)] else '0')
+        s.append('1' if row['ma_{0}_magnitude'.format(x)] else '0')
+
+        for y in periods:
+            if y <= x:
+                continue
+            s.append('1' if row['ma_{0}_crossover_{1}_bullish'.format(x, y)] else '0')
+            s.append('1' if row['ma_{0}_crossover_{1}_divergence'.format(x, y)] else '0')
+            s.append('1' if row['ma_{0}_crossover_{1}_magnitude'.format(x, y)] else '0')
+    # logging.debug('State: moving average {0}'.format(s))
+
+    s_string = ''.join(s)
+    logging.debug('State: {0}'.format(s_string))
+
+    return s_string
+
+
+def getReward(df, a, pip_mul):
+    a_trade, a_trailing = a.split('-')
+    logging.info('Reward: {0} with {1} stoploss'.format(a_trade, a_trailing))
+
+    entry = df.iloc[0]['close']
+    logging.debug('Reward: entry at {0:.4f}'.format(entry))
+    if a_trade == 'buy':
+        trail = -(float(a_trailing) / pip_mul)
+    elif a_trade == 'sell':
+        trail = (float(a_trailing) / pip_mul)
+    else:
+        raise Exception('Unknown trade type {0}'.format(a_trade))
+    take = entry + trail
+    logging.debug('Reward: trail at {0:.4f}'.format(trail))
+
+    ticks = -1
+    r = 0
+    for i, row in df.iterrows():
+        ticks += 1
+        logging.debug('Reward: {0} {1} {2:.4f} stop {3:.4f}'.format(a_trade, i, row['close'], take))
+        # buy
+        if a_trade == 'buy':
+            # exit?
+            if row['low'] < take:
+                logging.debug('Reward: take profit triggered: low [{0:.4f}] < take [{1:.4f}]'.format(row['low'], take))
+                r += take - entry
+                break
+            # new high?
+            if row['high'] + trail > take:
+                logging.debug('Reward: new high: high [{0:.4f}] + trail [{1:0.4f}] > take [{2:.4f}]'.format(row['high'], trail, take))
+                take = row['high'] + trail
+        # sell
+        if a_trade == 'sell':
+            # exit?
+            if row['high'] > take:
+                logging.debug('Reward: take profit triggered: high [{0:.4f}] > take [{1:.4f}]'.format(row['high'], take))
+                r += entry - take
+                break
+            # new low?
+            if row['low'] + trail < take:
+                logging.debug('Reward: new low: low [{0:.4f}] + trail [{1:0.4f}] < take [{2:.4f}]'.format(row['low'], trail, take))
+                take = row['low'] + trail
+
+    r -= ticks / pip_mul
+
+    return r, ticks
